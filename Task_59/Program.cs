47f7e25c5df78790a3da3f0b312b7f192a98684a
Kirill Mikhaylov <tytkaktyt463@gmail.com>
2023-09-17 14:41:03 +0300

    Task_59_final

--- Task_59/Program.cs
+++ Task_59/Program.cs
@@ -36,7 +36,11 @@ Console.WriteLine("");
 Console.WriteLine("");
 for(int i = 0; i < array.GetLength(0); i++){
     for(int j = 0; j < array.GetLength(1); j++){
-        Console.Write(array[i,j] + " ");
+        if(array[i,j] != 0){
+            Console.Write(array[i,j] + " ");
+        }
     }
+    if((array[i,i] != 0) || (array[i,i+1] != 0)){
     Console.WriteLine("");
+    }
 }
